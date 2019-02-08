b6832a5304bdace87fd433a0d949d6afc0562324
Des Dulianto <des.dulianto@bukalapak.com>
2019-02-08 18:15:09 +0700

    stack push & pop from list tail

--- src/05/stack.py
+++ src/05/stack.py
@@ -4,16 +4,31 @@ class Stack:
     def __init__(self):
         self.stack = LinkedList()
 
-    def push(self, value):
-        if len(self.stack) == 0:
-            self.stack.add(value)
-        else:
-            self.stack.insert(0, value)
 
+    # add di item pertama
+    #def push(self, value):
+    #    if len(self.stack) == 0:
+    #        self.stack.add(value)
+    #    else:
+    #        self.stack.insert(0, value)
+
+    # remove di item pertama
+    #def pop(self):
+    #    if len(self.stack) == 0: return None
+    #    value = self.stack.get(0)
+    #    self.stack.remove(0)
+    #    return value
+
+    # add ke akhir
+    def push(self, value):
+        self.stack.add(value)
+
+    # remove dari akhir
     def pop(self):
         if len(self.stack) == 0: return None
-        value = self.stack.get(0)
-        self.stack.remove(0)
+        last = self.stack.length-1
+        value = self.stack.get(last)
+        self.stack.remove(last)
         return value
 
     def __len__(self):
